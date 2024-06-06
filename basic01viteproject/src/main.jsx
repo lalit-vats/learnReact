dba05f280accaa771022a98d8303424fb8015ebb
lalit-vats <lalitvats863@gmail.com>
2024-06-06 15:51:31 +0530

    created custom react

--- basic01viteproject/src/main.jsx
+++ basic01viteproject/src/main.jsx
@@ -3,6 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import LalitAurReact from "./lalit.jsx";
 
+const anotherElement = (
+  <a href="https://google.com" target="_blenk">
+    visit google
+  </a>
+);
+
+const reactElement = React.createElement(
+  "a",
+  { href: "https:/google.com", target: "_blank" },
+  "click me to visit google !"
+);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
