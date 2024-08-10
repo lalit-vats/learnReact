cd34efd6468aeb624f379c8e085a588bb2e929ed
unknown <lalitvats863@gmail.com>
2024-08-10 19:02:00 +0530

    creqte new project

---
+++ 13megaBlog/src/conf/Conf.js
@@ -0,0 +1,8 @@
+const Conf  = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteCollectiontId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+export default Conf
